c69dcebed7f36ea1124814a0dc901bcd9aede76d
aalimova <aalimova1517@gmail.com>
2024-09-07 21:45:26 -0500

    created two middleware

---
+++ index.js
@@ -0,0 +1,34 @@
+//importing the express js module into application
+const express = require('express')
+
+//initializing the app using the express
+const app = express();
+
+function logger(req, res, next){
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+
+}
+
+const customHeader = (req, res, next) => {
+  res.setHeader('X-Custom-Header', 'Hello World');
+  next();
+}
+app.use(logger);
+app.use(customHeader);
+
+app.get("/", (req, res)=>{
+  console.log('Home page')
+  res.send("Home Page")
+})
+
+app.get('/users', (req, res) =>{
+  console.log("User Page")
+  res.send('User Page')
+})
+
+
+
+app.listen(3000, () => {
+  console.log("Server has started!");
+})
